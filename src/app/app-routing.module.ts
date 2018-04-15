9284c67378d14874e4abb1cc98c784b0074a80eb
Jeremy <jeremy.r.webster@intel.com>
2018-04-15 00:16:00 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,45 @@
-import { NgModule } from '@angular/core';
+import { NgModule,ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import{MainWindowComponent} from './main-window/main-window.component';
+import {CustomerComponent} from './customer/customer.component';
+import {EventComponent} from './event/event.component';
+import {MemberComponent} from './member/member.component';
+import {ScheduleComponent} from './schedule/schedule.component';
+import {TeamComponent} from './team/team.component';
+import {SignupComponent} from './signup/signup.component';
+import {LoginComponent} from './login/login.component';
+import {LogoutComponent} from './logout/logout.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {AboutComponent} from './about/about.component';
+
+const routes: Routes = [
+  {path: '',redirectTo:'/home',pathMatch:'full'},
+  {path: 'home',component:MainWindowComponent},
+  {path: 'about',component:AboutComponent},
+  {path: 'login',component:LoginComponent},
+  {path: 'logout',component:LogoutComponent},
+  {path: 'signup',component:SignupComponent},
+  {path: 'team',component:TeamComponent},
+  {path: 'schedule',component:ScheduleComponent},
+  {path: 'event',component:EventComponent},
+  {path: 'member',component:MemberComponent},
+  {path: 'customer',component:CustomerComponent},
+
+  // Commented paths are examples from another site that work and are necessary
+  // {path: 'tableau/:url',component:TableauComponent},
+  // {path: 'tableau_/:url',component:TableauComponent},
+  // {path: 'bobj/:indx', component:BobjComponent},
+  // {path: 'bobj_/:indx', component:BobjComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+
+//export const routes:ModuleWithProviders = RouterModule.forRoot(router,{enableTracing:false});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,45 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainWindowComponent } from './main-window/main-window.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MessageComponent } from './message/message.component';
+import { SignupComponent } from './signup/signup.component';
+import { EventComponent } from './event/event.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { TeamComponent } from './team/team.component';
+import { MemberComponent } from './member/member.component';
+import { CustomerComponent } from './customer/customer.component';
+import { SearchPipe } from './search.pipe';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopHeaderComponent,
+    FooterComponent,
+    MainWindowComponent,
+    SidebarComponent,
+    MessageComponent,
+    SignupComponent,
+    EventComponent,
+    ScheduleComponent,
+    TeamComponent,
+    MemberComponent,
+    CustomerComponent,
+    SearchPipe,
+    WelcomeComponent,
+    LoginComponent,
+    LogoutComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
